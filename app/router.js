e9c63216fcc7691daaa9647065422cb4cf5a5bbd
iyke <nderiony95@gmail.com>
2017-06-06 14:16:43 +0300

    add rental template

--- app/router.js
+++ app/router.js
@@ -3,12 +3,12 @@ import config from './config/environment';
 
 const Router = Ember.Router.extend({
   location: config.locationType,
-  rootURL: config.rootURL
 });
 
 Router.map(function() {
   this.route('about');
   this.route('contact');
+  this.route('rental'); {path: '/rental/:rental_id'});
 });
 
 export default Router;
